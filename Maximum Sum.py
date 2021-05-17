50f4efd44401bc88bb90a57083156365b60b6e2a
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-05-17 10:56:20 +0530

    Update Maximum Sum.py

--- Maximum Sum.py
+++ Maximum Sum.py
@@ -1,1 +1,13 @@
 
+n = int(input())
+arr = list(map(int, input().split()))
+sum_ = count = 0
+max_ = max(arr)
+for i in range(n):
+   if arr[i] >= 0:
+       sum_ += arr[i]
+       count += 1
+if count == 0:
+        print(max_, count+1)
+else:
+        print(sum_, count)
